7270174c4b825ded766837948eab8f11b70340be
Luis <fernando.ortellado@icloud.com>
2026-09-22 17:35:55 -0300

    calendario desplegable corregido

--- src/app/componentes/peliculas/peliculas.ts
+++ src/app/componentes/peliculas/peliculas.ts
@@ -1,10 +1,11 @@
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
-import { form, FormField, required, min, minLength } from '@angular/forms/signals';
+import { form, FormField, required, min, minLength, pattern } from '@angular/forms/signals';
 import { Pelicula, RESTRICCIONES_EDAD, IDIOMAS, FORMATOS, GENEROS } from '../../models/pelicula';
 import { PeliculasService } from '../../services/peliculas';
 import { FormatoDuracionPipe } from '../../pipes/formato-duracion-pipe-pipe';
 import { EdadColorDirective } from '../../directives/edad-color-directive';
+
 
 @Component({
   imports: [CommonModule, FormField, EdadColorDirective, TitleCasePipe, FormatoDuracionPipe],
@@ -43,6 +44,7 @@ export class Peliculas implements OnInit {
     required(schemaPath.idiomas_disponibles, { message: 'Seleccione un idioma' });
     required(schemaPath.formatos_disponibles, { message: 'Seleccione un formato' });
     required(schemaPath.generos, { message: 'Especifique al menos un género' });
+    pattern(schemaPath.fecha_fin_preventa, /^\d{2}\/\d{2}\/\d{4}$/, { message: 'Debe usar el formato DD/MM/AAAA' });
   });
 
   peliculas = signal<Pelicula[]>([]);
@@ -76,6 +78,8 @@ export class Peliculas implements OnInit {
       }));
   }
 
+
+
   cargarParaEditar(pelicula: Pelicula) {
     this.peliculaEditandoId.set(pelicula.id || null);
     
@@ -88,11 +92,29 @@ export class Peliculas implements OnInit {
       formatos_disponibles: pelicula.formatos_disponibles?.[0] || '',
       idiomas_disponibles: pelicula.idiomas_disponibles?.[0] || '',
       generos: pelicula.generos || [],
-      fecha_fin_preventa: pelicula.fecha_fin_preventa ? pelicula.fecha_fin_preventa.split('T')[0] : '',
+      fecha_fin_preventa: pelicula.fecha_fin_preventa ? pelicula.fecha_fin_preventa.split('T')[0].split('-').reverse().join('/') : '',      
       precio_preventa: pelicula.precio_preventa || 0
     });
 
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+    formatearFecha(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let valor = input.value.replace(/\D/g, ''); 
+
+    if (valor.length > 8) {
+      valor = valor.substring(0, 8); 
+    }
+
+    if (valor.length > 4) {
+      valor = `${valor.substring(0, 2)}/${valor.substring(2, 4)}/${valor.substring(4)}`;
+    } else if (valor.length > 2) {
+      valor = `${valor.substring(0, 2)}/${valor.substring(2)}`;
+    }
+
+    input.value = valor;
+    this.peliculaModel.update(m => ({ ...m, fecha_fin_preventa: valor }));
   }
 
   onSubmit(event: Event) {
@@ -114,7 +136,15 @@ export class Peliculas implements OnInit {
       };
 
       if (formValues.fecha_fin_preventa) {
-        payload.fecha_fin_preventa = formValues.fecha_fin_preventa;
+        const [dia, mes, anio] = formValues.fecha_fin_preventa.split('/');
+        const fechaIngresada = new Date(Number(anio), Number(mes) - 1, Number(dia));
+        const hoy = new Date();
+        hoy.setHours(0, 0, 0, 0);
+        if (fechaIngresada < hoy) {
+          alert('Error: La fecha de fin de preventa no puede ser anterior a la fecha actual')
+          return;
+        }
+        payload.fecha_fin_preventa = `${anio}-${mes}-${dia}`;
       }
 
       const idEditando = this.peliculaEditandoId();
